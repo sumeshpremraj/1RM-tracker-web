abcee79513dc6a07c689ed027c6d00c7a72ad57f
Sumesh Premraj <sumeshpremraj@gmail.com>
2020-02-23 17:18:05 +0530

    Add bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_moment import Moment
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,5 +15,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 moment = Moment(app)
+
+bootstrap = Bootstrap(app)
 
 from app import routes, models
