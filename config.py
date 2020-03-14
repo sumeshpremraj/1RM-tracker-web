8f6c30456e41c5370b31f80da0d167b74809402f
Sumesh Premraj <sumeshpremraj@gmail.com>
2020-03-14 19:52:55 +0530

    Add prod config class

--- config.py
+++ config.py
@@ -3,7 +3,25 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'bigyuuuugecovefefe'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
